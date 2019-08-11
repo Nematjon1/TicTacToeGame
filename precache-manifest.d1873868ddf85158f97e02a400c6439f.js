b75b667cd8b70fe786fe5e44c2de4fa9fcfe9fbf
Nematjon <frank.sport.93@gmail.com>
2019-08-11 14:12:59 +0300

    Updates

---
+++ precache-manifest.d1873868ddf85158f97e02a400c6439f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1031ed6a91e26a80816f",
+    "url": "/TicTacToeGame/static/js/runtime~main.1031ed6a.js"
+  },
+  {
+    "revision": "f1a116c2626becb6e3bd",
+    "url": "/TicTacToeGame/static/js/main.f1a116c2.chunk.js"
+  },
+  {
+    "revision": "5152063aed6ca8db98bd",
+    "url": "/TicTacToeGame/static/js/1.5152063a.chunk.js"
+  },
+  {
+    "revision": "f1a116c2626becb6e3bd",
+    "url": "/TicTacToeGame/static/css/main.7a0541ef.chunk.css"
+  },
+  {
+    "revision": "84637842b3be9281ef69bb21863f5747",
+    "url": "/TicTacToeGame/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c4552d2d49c28647163172e2fbb64bd0.js"
+  "/TicTacToeGame/precache-manifest.d1873868ddf85158f97e02a400c6439f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/TicTacToeGame/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
